7a09011a4e41b5392675d15d0c3dff0785efe006
YaBoiEd <60477909+YaBoiEd@users.noreply.github.com>
2023-07-09 17:45:16 +0100

    Hard carry
    *I* finished the project.

--- TowerOffence/Assets/Scripts/ButtonLibrary.cs
+++ TowerOffence/Assets/Scripts/ButtonLibrary.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class ButtonLibrary : MonoBehaviour
 {
+    [SerializeField] private GameObject currencyText;
+
+    private void Start()
+    {
+        currencyText.GetComponent<TMP_Text>().text = $"Money: £{GameManager.Instance.Money}";
+    }
     public void Purchase(GameObject playerPrefab)
     {
-        GameObject newUnit = Instantiate(playerPrefab, GameObject.FindWithTag("LevelPath").transform.GetChild(0));
-        GameManager.Instance.Units.Add(newUnit);
+        if ((GameManager.Instance.Money - playerPrefab.GetComponent<Unit>().Cost) >= 0)
+        {
+            GameManager.Instance.Money -= playerPrefab.GetComponent<Unit>().Cost;
+            currencyText.GetComponent<TMP_Text>().text = $"Money: £{GameManager.Instance.Money}";
+            GameObject newUnit = Instantiate(playerPrefab, GameObject.FindWithTag("LevelPath").transform.GetChild(0));
+            GameManager.Instance.Units.Add(newUnit);
+        }
     }
 }
